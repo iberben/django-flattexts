13d7e10b8963a2979bc44d8342918615ccf25796
Jakub Paczkowski <jakub@paczkowski.eu>
2015-02-06 10:59:47 +0100

    migrations hell, woohoo!

--- flattexts/migrations/0001_initial.py
+++ flattexts/migrations/0001_initial.py
@@ -1,35 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-from south.db import db
-from django.db import models
-from flattexts.models import *
+from django.db import models, migrations
 
-class Migration:
-    
-    def forwards(self, orm):
-        
-        # Adding model 'FlatText'
-        db.create_table('flattexts_flattext', (
-            ('id', orm['flattexts.FlatText:id']),
-            ('slug', orm['flattexts.FlatText:slug']),
-            ('content', orm['flattexts.FlatText:content']),
-        ))
-        db.send_create_signal('flattexts', ['FlatText'])
-        
-    
-    
-    def backwards(self, orm):
-        
-        # Deleting model 'FlatText'
-        db.delete_table('flattexts_flattext')
-        
-    
-    
-    models = {
-        'flattexts.flattext': {
-            'content': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
-        }
-    }
-    
-    complete_apps = ['flattexts']
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FlatText',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('slug', models.SlugField(verbose_name='slug')),
+                ('content', models.TextField(verbose_name='content')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
